44e8372d43011710f26f0c31b2384c98067e0069
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2023-04-25 08:29:26 -0300

    add: fifth row showing pixel difference

--- figures/figure_comparing_true_and_prediction.py
+++ figures/figure_comparing_true_and_prediction.py
@@ -1,4 +1,5 @@
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageChops
 import pandas as pd
 import texfig  # import texfig first to configure Matplotlib's backend
 
@@ -10,9 +11,9 @@ def main():
     dataframe_original = pd.read_csv("dataset.csv")
     dataframe_unet = pd.read_csv("dataset_unet.csv")
 
-    ratio = 1.0
+    ratio = 1.25
 
-    fig, ax = texfig.subplots(ratio=ratio, nrows=4, ncols=4)
+    fig, ax = texfig.subplots(ratio=ratio, nrows=5, ncols=4)
 
     for i, index in enumerate(SELECTED_INDICES):
         path = dataframe_original["images_vel_x"][index]
@@ -22,19 +23,36 @@ def main():
         image_vel_y = Image.open(path)
 
         path = dataframe_unet["images_unet"][index]
-        image_pressure_unet = Image.open(path)
+        image_pressure_unet = Image.open(path).convert("L")
 
         path = dataframe_original["images_pressure"][index]
-        image_pressure_original = Image.open(path)
+        image_pressure_original = Image.open(path).convert("L")
+
+        # buffer1 = np.asarray(image_pressure_original.resize(image_pressure_unet.size))
+        # buffer2 = np.asarray(image_pressure_unet)
+        # buffer3 = np.ones_like(buffer1) - buffer1 + buffer2
+        # difference = Image.fromarray(buffer3)
+        difference = ImageChops.difference(
+            image_pressure_original.resize(image_pressure_unet.size),
+            image_pressure_unet,
+        )
+        difference_gray = difference.convert("L")
 
         ax[0, i].imshow(image_vel_x)
         ax[1, i].imshow(image_vel_y)
-        ax[2, i].imshow(image_pressure_unet)
-        ax[3, i].imshow(image_pressure_original)
+        ax[2, i].imshow(image_pressure_unet, cmap="gray", vmin=0, vmax=255)
+        ax[3, i].imshow(
+            image_pressure_original.resize(image_pressure_unet.size),
+            cmap="gray",
+            vmin=0,
+            vmax=255,
+        )
+        ax[4, i].imshow(difference_gray, cmap="gray_r", vmin=0, vmax=255)
         ax[0, i].axis("off")
         ax[1, i].axis("off")
         ax[2, i].axis("off")
         ax[3, i].axis("off")
+        ax[4, i].axis("off")
 
         ax[0, 0].text(
             0.06,
@@ -69,6 +87,15 @@ def main():
             r"True $Cp$",
             rotation="vertical",
             transform=ax[3, 0].transAxes,
+            ha="center",
+            size=10,
+        )
+        ax[4, 0].text(
+            0.06,
+            0.25,
+            r"Difference",
+            rotation="vertical",
+            transform=ax[4, 0].transAxes,
             ha="center",
             size=10,
         )
